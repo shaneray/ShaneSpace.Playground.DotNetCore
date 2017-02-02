f997cd3b4e1e0a758934e7050dbe4cdd98db728a
Shane Ray <shane.ray87@gmail.com>
2017-02-02 17:35:05 -0500

    day 1 - created solution and projec added some initial nuget packages and configuration.

---
+++ src/ShaneSpace.Playground.DonNetCore/Startup.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using React.AspNet;
+
+namespace ShaneSpace.Playground.DonNetCore
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services
+                .AddSingleton<IHttpContextAccessor, HttpContextAccessor>()
+                .AddReact()
+                .AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory
+                .AddConsole(Configuration.GetSection("Logging"))
+                .AddDebug();
+
+            // Initialise ReactJS.NET. Must be before static files.
+            app.UseReact(config =>
+            {
+                // If you want to use server-side rendering of React components,
+                // add all the necessary JavaScript files here. This includes
+                // your components as well as all of their dependencies.
+                // See http://reactjs.net/ for more information. Example:
+                config
+                .SetReuseJavaScriptEngines(true)
+                  .AddScript("~/js/playground.jsx")
+                  .SetUseDebugReact(true);
+                //  .AddScript("~/Scripts/Second.jsx");
+
+                // If you use an external build too (for example, Babel, Webpack,
+                // Browserify or Gulp), you can improve performance by disabling
+                // ReactJS.NET's version of Babel and loading the pre-transpiled
+                // scripts. Example:
+                //config
+                //  .SetLoadBabel(false)
+                //  .AddScriptWithoutTransform("~/Scripts/bundle.server.js");
+            });
+
+            app.UseDefaultFiles().UseStaticFiles().UseMvc();
+        }
+    }
+}
---
+++ src/ShaneSpace.Playground.DonNetCore/wwwroot/js/playground.jsx
@@ -0,0 +1,37 @@
+﻿var CommentBox = React.createClass({
+    displayName: 'CommentBox',
+    render: function () {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
+            </div>
+        );
+    }
+});
+
+var CommentList = React.createClass({
+    render: function () {
+        return (
+            <div className="commentList">
+                Hello, world! I am a CommentList.
+      </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function () {
+        return (
+            <div className="commentForm">
+                Hello, world! I am a CommentForm.
+      </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    React.createElement(CommentBox, null),
+    document.getElementById('content')
+);
